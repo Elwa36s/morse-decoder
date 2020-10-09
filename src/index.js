781f2fd604500845f814c1a845f51005e59b1624
Elwa36s <24dds08@gmail.com>
2020-10-09 13:06:03 +0300

    feat: finish morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let i = 0;
+    let word = [];
+    let result = "";
+    for (let j = 10; j <= expr.length; j += 10) {
+        array.push(expr.slice(i, j));
+        i += 10;
+    }
+    for (let j = 0; j < array.length; j++) {
+        let letter = array[j].toString();
+        let str = '';
+        if (letter === '**********') str += ' ';
+        for (let k = 0; k <= 8; k += 2) {
+            let symb = letter[k] + letter[k + 1];
+            if (symb === '10') {
+                str += '.';
+            }
+            if (symb === '11') {
+                str += '-';
+            }
+        }
+        word.push(str);
+    }
+    for (let j = 0; j < word.length; j++) {
+        let morseCode = word[j];
+        if (morseCode === ' ') {
+            result += ' ';
+        } else {
+        result += MORSE_TABLE[morseCode];
+     }
+    }
+    return result;
 }
 
 module.exports = {
